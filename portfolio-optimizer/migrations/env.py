28c6ad4c39764ccb4c24965ac4f97c9fec4f62d1
Gregory Parent <gregparent2006@gmail.com>
2025-03-23 14:12:39 -0400

    Set up alembic

---
+++ portfolio-optimizer/migrations/env.py
@@ -0,0 +1,57 @@
+from __future__ import with_statement
+import logging
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config, create_engine
+from sqlalchemy import pool
+from alembic import context
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = "localhost"  
+DB_PORT = "5432"  
+DB_NAME = "portfolio_optimizer"
+
+database_url = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+# Import your model’s MetaData object here
+from app.models import db  # This should be your SQLAlchemy instance
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Setup logging
+fileConfig(config.config_file_name)
+logger = logging.getLogger('alembic.runtime.migration')
+
+# Use your application's MetaData object
+target_metadata = db.metadata
+
+# Add your database URL here
+config.set_main_option('sqlalchemy.url', database_url)
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    """Run migrations in 'online' mode."""
+    engine = create_engine(config.get_main_option("sqlalchemy.url"))
+    connection = engine.connect()
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
