a06023532370faf27f897185eb0cd273d986e14b
Gregory Parent <gregparent2006@gmail.com>
2025-03-23 13:25:42 -0400

    Finished setting up flask backend and using alpha vantage to store data in timescaleDB

---
+++ portfolio-optimizer/app/models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class OHLCData(db.Model):
+    __tablename__ = 'ohlc_data'
+    
+    id = db.Column(db.Integer, primary_key=True) 
+    ticker = db.Column(db.String(10), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    open = db.Column(db.Float, nullable=False)
+    high = db.Column(db.Float, nullable=False)
+    low = db.Column(db.Float, nullable=False)
+    close = db.Column(db.Float, nullable=False)
+    volume = db.Column(db.Integer, nullable=False)
--- portfolio-optimizer/app/routes.py
+++ portfolio-optimizer/app/routes.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.models import OHLCData
+from app import db
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
-@main.route('/')
-def index():
-    return {"message": "Flask backend is running"}
+@main.route("/")
+def home():
+    return jsonify({"message": "Flask Backend is Running!"})
+
+@main.route("/ohlc/<ticker>", methods=["GET"])
+def get_ohlc(ticker):
+    data = OHLCData.query.filter_by(ticker=ticker).all()
+    return jsonify([{
+        'id': d.id,
+        "ticker": d.ticker,
+        "timestamp": d.timestamp,
+        "open": d.open,
+        "high": d.high,
+        "low": d.low,
+        "close": d.close,
+        "volume": d.volume
+    } for d in data])
