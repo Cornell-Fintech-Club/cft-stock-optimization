0f8d2a592ea1e53ed884fbdb4819ee4eda0bee4a
Gregory Parent <gregparent2006@gmail.com>
2025-03-26 09:00:44 -0400

    Improved API testing script, allowing for date ranges and entering in specific stocks

--- portfolio-optimizer/test_api.py
+++ portfolio-optimizer/test_api.py
@@ -1,5 +1,15 @@
 import requests
 
-url = 'http://127.0.0.1:5000/ohlc/APPL'
-response = requests.get(url)
-print(response.json())
+ticker = input("Request a Ticker:")
+url = 'http://127.0.0.1:5000/ohlc/'+ticker
+answer = input("Would you like to Reqeust Specific Time Range (y/n): ").lower()
+
+if answer=="y" or answer == "yes":
+    print("Example Format: 2024-01-01")
+    startTime = input("Request a Start Time: ")
+    endTime = input("Request an End Time: ")
+    url += '?start_date=' + startTime + '&end_date='+ endTime 
+    print(url)
+# http://127.0.0.1:5000/ohlc/AAPL?start_date=2022-01-01&end_date=2022-03-01
+else:
+    print(url)
